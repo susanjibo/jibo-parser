fd1d99ef59e2a6ae78052130f0c6fbcc7ae44f55
Ramon <ramon.prieto@jibo.com>
2015-12-06 19:06:21 -0800

    v0.7.2, adding arch besides platform in figuring out what jibo-nlu-js package to download

--- install.js
+++ install.js
@@ -8,17 +8,12 @@ var mkdir = require('mkdirp');
 var pathExists = require('path-exists');
 var extract = require('extract-zip');
 
-var libs = path.resolve(__dirname, 'deps', process.platform, 'lib');
+var libs = path.resolve(__dirname, 'deps', process.platform, process.arch,'lib');
 
-var downloadUrl = 'http://repository.jibo.com/sdk/jibo-nlu-js/';
-var platform;
-if(process.platform === 'win32' || process.platform === 'win64') {
-    platform = 'win32';
-}
-else {
-    platform = process.platform;
-}
-var target = 'jibo-nlu-js-v' + version + '-' + platform + '.zip';
+var downloadUrl = 'http://repository.jibo.com/nlu/jibo-nlu-js/';
+var platform = process.platform;
+var arch = process.arch
+var target = 'jibo-nlu-js-v' + version + '-' + platform + '-' + arch + '.zip';
 var targetDir = 'jibo-nlu-js';
 downloadUrl += target;
 
