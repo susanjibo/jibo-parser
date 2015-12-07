70599bbf01fa4e954616105abcc67511b89cb6db
Ramon <ramon.prieto@jibo.com>
2015-12-07 14:59:15 -0800

    Forcing ia32 forwindows, x64 for mac, x64 for linux

--- install.js
+++ install.js
@@ -13,6 +13,16 @@ var libs = path.resolve(__dirname, 'deps', process.platform, process.arch,'lib')
 var downloadUrl = 'http://repository.jibo.com/nlu/jibo-nlu-js/';
 var platform = process.platform;
 var arch = process.arch
+if (platform == 'win32') {
+    // Windows atom supports only 32 bits (as of 1.2.4)
+    arch = 'ia32';
+} else if (platform == 'linux') {
+    // Linux atom supports only 64 bits (as of 1.2.4)
+    arch = 'x64';
+} else if (platform == 'darwin') {
+    // darwin macos comes only in x64 flavor 
+    arch = 'x64';
+}
 var target = 'jibo-nlu-js-v' + version + '-' + platform + '-' + arch + '.zip';
 var targetDir = 'jibo-nlu-js';
 downloadUrl += target;
