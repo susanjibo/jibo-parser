79a7f19a2552fdfc4b9cf0d021436bf02b49e206
Jonathan Ross <jonathan@jibo.com>
2015-12-22 15:50:13 -0500

    Fided path resolution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var nlu = module.require('../build/Release/jsjibonlu');
+var nlu = module.require('./build/Release/jsjibonlu');
 var path = require('path');
 var dataDirectory = path.join(__dirname, 'deps', 'data');
 nlu.set_data_dir(dataDirectory);
